b569a8472237e630cbfab6e42f213f4ff006d2c1
NewLine2012xD <Carlos.Calderon01@outlook.com>
2023-11-18 10:38:57 -0500

    release 0.0

---
+++ release/release 0.0.js
@@ -0,0 +1,39 @@
+"use strict";
+
+// ----- // ----- // Variables // ----- // ----- //
+
+const ascii_lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const ascii_uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const digits          = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const hexdigits       = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "A", "B", "C", "D", "E", "F"];
+const octdigits       = ["0", "1", "2", "3", "4", "5", "6", "7"];
+const punctuation     = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~", '"',];
+
+// del estandar "punctuation" me falto "\" no supe como ponerlo.
+
+// ----- // ----- // Procesos // ----- // ----- //
+
+let random_ascii_lowercase = ascii_lowercase[Math.floor(Math.random() * ascii_lowercase.length)]
+let random_ascii_uppercase = ascii_uppercase[Math.floor(Math.random() * ascii_uppercase.length)]
+let random_punctuation = punctuation[Math.floor(Math.random() * punctuation.length)]
+let random_hexdigits = hexdigits[Math.floor(Math.random() * hexdigits.length)]
+let random_octdigits = octdigits[Math.floor(Math.random() * octdigits.length)]
+let random_digits = digits[Math.floor(Math.random() * digits.length)]
+
+// ----- // ----- // imprimir // ----- // ----- //
+
+console.log("// ----- // ----- // ----- // ----- // ----- // ----- //");
+console.log(random_ascii_lowercase);
+console.log(random_ascii_uppercase);
+console.log(random_digits);
+console.log(random_hexdigits);
+console.log(random_octdigits);
+console.log(random_punctuation);
+console.log("// ----- // ----- // ----- // ----- // ----- // ----- //");
+
+/*
+    Biliografia -->
+
+    https://docs.python.org/3/library/string.html
+
+*/
